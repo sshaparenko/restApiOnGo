57cfd4a932aed0e3273337351d31e76efc198551
sshaparenko <zshaparenkoz@gmail.com>
2024-09-27 18:16:43 +0300

    chore: adding error handling by adding error wrapping and performing additional checks

--- pkg/utils/faker.go
+++ pkg/utils/faker.go
@@ -1,14 +1,19 @@
 package utils
 
-import "github.com/bxcodec/faker/v3"
+import (
+	"fmt"
 
+	"github.com/bxcodec/faker/v3"
+)
+
+// CreateFaker creates faker data for provided type
 func CreateFaker[T any]() (T, error) {
 	//create variable ro store faker data
 	var fakerData *T = new(T)
 
 	err := faker.FakeData(fakerData)
 	if err != nil {
-		return *fakerData, err
+		return *fakerData, fmt.Errorf("generating fake data in utils.CreateFaker: %w", err)
 	}
 
 	return *fakerData, nil
